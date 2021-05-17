0c9ec3b32163f0402c033979c3ba685d67bec644
Kuba <dzcapital@gmail.com>
2021-05-16 22:49:00 -0400

    Buttons

--- components/carItem/index.js
+++ components/carItem/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { View, Text, ImageBackground } from "react-native";
 import styles from "./styles";
+import StyledButton from "../StyledButton";
 
 const CarItem = (props) => {
   return (
     <View style={styles.frontContainer}>
       <ImageBackground
-        source={require("../../assets/images/main-hero.jpg")}
+        source={require("../../assets/images/ModelS.jpeg")}
         style={styles.backgroundImage}
       />
 
@@ -14,6 +15,21 @@ const CarItem = (props) => {
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Order Online for Touchless Delivery</Text>
       </View>
+
+      <StyledButton
+        type="primary"
+        content={"Custom Order"}
+        onPress={() => {
+          console.warn("Button was pressed");
+        }}
+      />
+      <StyledButton
+        type="secondary"
+        content={"Existing Inventory"}
+        onPress={() => {
+          console.warn("2nd Button was pressed");
+        }}
+      />
     </View>
   );
 };
